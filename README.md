b0758488902b0cfd1a57b2df759e8453df9aae0f
LLN <lev.kudryashov1994@gmail.ru>
2014-06-04 18:43:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Project1
+=======
+My 1st project
---
+++ src/main/java/com/epam/lk/Pythagore.java
@@ -0,0 +1,18 @@
+package com.epam.lk;
+
+public class Pythagore {
+    public Pythagore() {
+    }
+
+    public static void main(String[] args) {
+        double a = 3;
+
+        double b = 4;
+
+        double c;
+
+        c = Math.sqrt(a* a + b* b);
+
+        System.out.println ("third side = "+ c);
+    }
+}
